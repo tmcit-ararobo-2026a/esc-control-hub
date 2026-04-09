b95b2891ee744f30f5e01a16eac68caa368d7b69
aiba-gento <8gn24gn25@gmail.com>
2026-04-09 21:13:19 +0900

    fix:formatとappを修正

--- esc_hub/include/esc_hub/app.hpp
+++ esc_hub/include/esc_hub/app.hpp
@@ -1,52 +1,20 @@
 #pragma once
 
-#include <cstdint>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "main.h"
 
-class mmainFW
-{
-public:
-    void setup();
-    void loop();
-    inline void Get_Encoder();
+/**
+ * @brief 最初に１回だけ実行される関数
+ */
+void setup();
 
-    volatile int64_t Encoder_Count_Port1;
-    volatile int64_t Encoder_Count_Port2;
-    volatile int64_t Encoder_Count_Port3;
-    volatile int64_t Encoder_Count_Port4;
-
-    volatile bool NVIC_1Hz;
-    volatile bool NVIC_1kHz;
-    volatile bool NVIC_10kHz;
-    volatile bool NVIC_main_FDCAN;
-    volatile bool NVIC_esc_CAN;
-
-    typedef struct {
-        uint32_t Id;
-        uint8_t Len;
-        uint8_t Data[64];
-        uint8_t* Data_p;
-    } fdcan_frame_callback;
-
-    fdcan_frame_callback main_fdcan_callback;
-    fdcan_frame_callback esc_fdcan_callback;
-};
-
-extern mmainFW main_FW;
-
-extern FDCAN_HandleTypeDef hfdcan1;
-extern FDCAN_HandleTypeDef hfdcan2;
-
-extern XSPI_HandleTypeDef hospi1;
-
-extern RTC_HandleTypeDef hrtc;
-
-extern TIM_HandleTypeDef htim1;
-extern TIM_HandleTypeDef htim2;
-extern TIM_HandleTypeDef htim3;
-extern TIM_HandleTypeDef htim4;
-extern TIM_HandleTypeDef htim6;
-extern TIM_HandleTypeDef htim8;
-
-extern PCD_HandleTypeDef hpcd_USB_DRD_FS;
+/**
+ * @brief setup関数終了後実行され続けるループ関数
+ */
+void loop();
+#ifdef __cplusplus
+}
+#endif
--- esc_hub/include/esc_hub/fdcan_driver.hpp
+++ esc_hub/include/esc_hub/fdcan_driver.hpp
@@ -5,35 +5,34 @@
 #include "gn10_can/drivers/driver_interface.hpp"
 #include "stm32h5xx_hal_fdcan.h"
 
-maidui3_hal::fdcan::FDCANfunction;
+namespace gn10_can {
+namespace drivers {
+class DriverHALFDCAN : public ICanDriver
+{
+public:
+    DriverHALFDCAN(FDCAN_HandleTypeDef* hfdcanx) : hfdcanx_(hfdcanx) {}
 
-namespace gn10_can{
-    namespace drivers{
-        class DriverHALFDCAN : public ICanDriver{
-            public:
-                DriverHALFDCAN(FDCAN_HandleTypeDef* hfdcanx) : hfdcanx_(hfdcanx){}
+    bool init(maidui3_hal::fdcan::Fifo_Type fifo_);
+    bool send(const CANFrame& frame) override;
+    bool receive(CANFrame& out_frame) override;
 
-                bool init(maidui3_hal::fdcan::Fifo_Type fifo_);
-                bool send(const CANFrame& frame) override;
-                bool receive(CANFrame& out_frame) override;
+private:
+    FDCAN_HandleTypeDef* hfdcanx_;
 
-            private:
-                FDCAN_HandleTypeDef* hfdcanx_;
+    maidui3_hal::fdcan::FDCANfunction W2_;
+};
+}  // namespace drivers
+}  // namespace gn10_can
 
-                maidui3_hal::fdcan::FDCANfunction W2_;
-        };
-    }
-}
-
-namespace maidui3_hal{
-    namespace fdcan{
-        class FDCANfunction{
-            public:
-                fdcan_state_TypeDef Timeout(Enable_and_Disable state_);
-                fdcan_state_TypeDef beginning(Enable_and_Disable state_);
-                fdcan_state_TypeDef Tx_Callback(Enable_and_Disable state_);
-                fdcan_state_TypeDef Rx_Callback(Enable_and_Disable state_);
-
-        };
-    }
-}
+namespace maidui3_hal {
+namespace fdcan {
+class FDCANfunction
+{
+public:
+    fdcan_state_TypeDef Timeout(Enable_and_Disable state_);
+    fdcan_state_TypeDef beginning(Enable_and_Disable state_);
+    fdcan_state_TypeDef Tx_Callback(Enable_and_Disable state_);
+    fdcan_state_TypeDef Rx_Callback(Enable_and_Disable state_);
+};
+}  // namespace fdcan
+}  // namespace maidui3_hal
